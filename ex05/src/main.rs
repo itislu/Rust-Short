d7d1906814464a2738c542a0e9fe1d53fb37986b
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-10-07 21:38:49 +0200

    [ex05] test: Add unit tests

--- ex05/src/main.rs
+++ ex05/src/main.rs
@@ -61,10 +61,89 @@ fn num_days_in_month(year: u32, month: u32) -> u32 {
     }
 }
 
+#[cfg(test)]
+mod num_days_in_month {
+    use super::*;
+
+    #[test]
+    fn test_leap_year_february() {
+        let result = num_days_in_month(4, 2);
+        assert_eq!(result, 29);
+        let result = num_days_in_month(1600, 2);
+        assert_eq!(result, 29);
+    }
+
+    #[test]
+    fn test_common_year_february() {
+        let result = num_days_in_month(5, 2);
+        assert_eq!(result, 28);
+        let result = num_days_in_month(1601, 2);
+        assert_eq!(result, 28);
+    }
+
+    #[test]
+    fn test_leap_year_other() {
+        let result = num_days_in_month(4, 1);
+        assert_eq!(result, 31);
+        let result = num_days_in_month(1600, 12);
+        assert_eq!(result, 31);
+    }
+
+    #[test]
+    fn test_common_year_other() {
+        let result = num_days_in_month(5, 1);
+        assert_eq!(result, 31);
+        let result = num_days_in_month(1601, 12);
+        assert_eq!(result, 31);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_month() {
+        let _result = num_days_in_month(5, 13);
+        let _result = num_days_in_month(1600, 112);
+    }
+}
+
 fn is_leap_year(year: u32) -> bool {
     if year == 0 {
         panic!()
     } else {
         (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+    }
+}
+
+#[cfg(test)]
+mod is_leap_year {
+    use super::*;
+
+    #[test]
+    fn test_leap_year() {
+        let result = is_leap_year(1600);
+        assert_eq!(result, true);
+        let result = is_leap_year(2004);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_common_year() {
+        let result = is_leap_year(1500);
+        assert_eq!(result, false);
+        let result = is_leap_year(2003);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_leap_year_month() {
+        let result = is_leap_year(1500);
+        assert_eq!(result, false);
+        let result = is_leap_year(2003);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_year_zero() {
+        let _result = is_leap_year(0);
     }
 }
