e5af028290f657b6631b57f4e7bdcb5f21b2e43c
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-10-11 13:16:24 +0200

    [ex00] fix: Ignore Result entirely

--- ex00/src/main.rs
+++ ex00/src/main.rs
@@ -2,8 +2,6 @@ use std::io::Write;
 
 fn main() {
     for i in 1..=10 {
-        if let Err(_) = writeln!(std::io::stdout(), "{i}") {
-            break
-        }
+        let _ = writeln!(std::io::stdout(), "{i}");
     }
 }
