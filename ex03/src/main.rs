fc508bc37a873476436dba2194ef3bdbcedffef9
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-10-11 21:15:11 +0200

    [ex03] feat: Implement simple cmd executor

---
+++ ex03/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::io;
+use std::io::BufRead;
+use std::os::unix::process::CommandExt;
+use std::process;
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+
+    let mut input = String::new();
+    while io::stdin().lock().read_line(&mut input).unwrap() != 0 {
+        args.push(input.trim_end_matches('\n').to_string());
+        input.clear();
+    }
+
+    process::Command::new(args.remove(0)).args(args).exec();
+}
